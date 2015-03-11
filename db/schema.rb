6583b33665135e7790e32eb53ffa26c4fecef371
Luis <kilinkala@gmail.com>
2015-03-11 16:18:51 +0100

    Actualizado sistema usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150310163420) do
 
-  create_table "statuses", force: true do |t|
+  create_table "statuses", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "profile_name"
